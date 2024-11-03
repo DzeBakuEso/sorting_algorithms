a52fa686093821146a0e68154d6c18185be8cd41
DzeBakuEso <dzeblebaku@gmail.com>
2024-11-03 09:26:34 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,58 @@
-#include "sort.h"
-#include <stdio.h> /*Include stdio.h for printf*/
+#include <sort.h>
+#include <stdio.h>
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using
- *               the bubble sort algorithm.
+ * bubble_sort - Sorts an array using the bubble sort algorithm.
  * @array: The array to be sorted.
- * @size: The number of elements in the array.
+ * @size: The size of the array.
 */
-void bubble_sort(int *array, size_t size)
+void bubble_sort(int array[], int size)
 {
-	size_t i, j;
-	int temp;
-
-	if (array == NULL || size < 2)
-		return;
+	int i, j, temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				/* Swap*/
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 			}
 		}
-/* Print the array after each pass*/
-		print_array(array, size);
 	}
+}
+
+/**
+ * print_array - Prints the elements of an array.
+ * @array: The array to print.
+ * @size: The size of the array.
+*/
+void print_array(int array[], int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+*/
+int main(void)
+{
+	int arr[] = {64, 34, 25, 12, 22, 11, 90};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	bubble_sort(arr, n);
+	print_array(arr, n);
+
+	return (0);
 }
